7945c0f997def9a75b4f37459e345aecdf883816
levi <levi@gmail.com>
2022-05-17 09:29:02 +0530

    nwe

---
+++ src/test/java/org/mugi/mugi.java
@@ -0,0 +1,15 @@
+package org.mugi;
+
+public class mugi {
+	
+	private void step1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void step2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
